3ba839c835c666855f396e71658faf4bac861c3d
Enzo Hu <enzohx@163.com>
2019-04-24 16:21:49 +0800

    improvement - v-cloak

--- account/http.py
+++ account/http.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Tencent is pleased to support the open source community by making 蓝鲸智云(BlueKing) available.
-Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
-Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
-You may obtain a copy of the License at http://opensource.org/licenses/MIT
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
-an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and limitations under the License.
-
 请求登录的http基础方法
 
 Rules:
@@ -35,7 +27,7 @@ def _gen_header():
 def _http_request(method, url, headers=None, data=None):
     try:
         if method == "GET":
-            resp = requests.get(url=url, headers=headers, params=data) # TODO verify=False 参数影响???
+            resp = requests.get(url=url, headers=headers, params=data)
         elif method == "HEAD":
             resp = requests.head(url=url, headers=headers)
         elif method == "POST":
